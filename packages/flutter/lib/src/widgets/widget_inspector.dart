ac7a35a6b67a7261ffb3d7aea1d786a2ad25bf66
Jacob Richman <jacobr@google.com>
2018-01-17 17:26:25 -0800

    Inspector robustness fixes. Avoid cases where setState was called on disposed object. Avoid cases where the inspector wouldn't render as it was called after the first frame rendered and no other frames were rendered.

--- packages/flutter/lib/src/widgets/widget_inspector.dart
+++ packages/flutter/lib/src/widgets/widget_inspector.dart
@@ -13,6 +13,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'basic.dart';
 import 'binding.dart';
@@ -230,6 +231,13 @@ class WidgetInspectorService {
     return id;
   }
 
+  /// Returns whether the application has rendered its first frame and it is
+  /// appropriate to display the Widget tree in the inspector.
+  bool isWidgetTreeReady([String groupName]) {
+    return WidgetsBinding.instance != null &&
+           WidgetsBinding.instance.debugDidSendFirstFrameEvent;
+  }
+
   /// Returns the Dart object associated with a reference id.
   ///
   /// The `groupName` parameter is not required by is added to regularize the
@@ -240,8 +248,9 @@ class WidgetInspectorService {
       return null;
 
     final _InspectorReferenceData data = _idToReferenceData[id];
-    if (data == null)
-      throw new FlutterError('Id does not exist');
+    if (data == null) {
+      throw new FlutterError('Id does not exist.');
+    }
     return data.object;
   }
 
@@ -311,7 +320,16 @@ class WidgetInspectorService {
         selection.current = object;
       }
       if (selectionChangedCallback != null) {
-        selectionChangedCallback();
+        if (WidgetsBinding.instance.schedulerPhase == SchedulerPhase.idle) {
+          selectionChangedCallback();
+        } else {
+          // It isn't safe to trigger the selection change callback if we are in
+          // the middle of rendering the frame.
+          SchedulerBinding.instance.scheduleTask(
+            selectionChangedCallback,
+            Priority.touch,
+          );
+        }
       }
       return true;
     }
@@ -511,23 +529,43 @@ class _WidgetInspectorState extends State<WidgetInspector>
   /// as selecting the edge of the bounding box.
   static const double _kEdgeHitMargin = 2.0;
 
+  InspectorSelectionModeChangedCallback _selectionModeChangedCallback;
+  InspectorSelectionChangedCallback _selectionChangedCallback;
+
   @override
   void initState() {
     super.initState();
-    WidgetInspectorService.instance.selectionChangedCallback = () {
+
+    _selectionChangedCallback = () {
       setState(() {
         // The [selection] property which the build method depends on has
         // changed.
       });
     };
-    WidgetInspectorService.instance.selectionModeChangedCallback = (bool mode) {
+
+    _selectionModeChangedCallback = (bool mode) {
       if (mode != isSelectMode) {
         setState(() {
           isSelectMode = mode;
         });
       }
     };
+    assert(WidgetInspectorService.instance.selectionModeChangedCallback == null);
+    WidgetInspectorService.instance.selectionModeChangedCallback = _selectionModeChangedCallback;
 
+    assert(WidgetInspectorService.instance.selectionChangedCallback == null);
+    WidgetInspectorService.instance.selectionChangedCallback = _selectionChangedCallback;
+  }
+
+  @override
+  void dispose() {
+    if (WidgetInspectorService.instance.selectionChangedCallback == _selectionChangedCallback) {
+      WidgetInspectorService.instance.selectionChangedCallback = null;
+    }
+    if (WidgetInspectorService.instance.selectionModeChangedCallback == _selectionModeChangedCallback) {
+      WidgetInspectorService.instance.selectionModeChangedCallback = null;
+    }
+    super.dispose();
   }
 
   bool _hitTestHelper(
